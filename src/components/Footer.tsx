62b42213b27c36a70f82716a06675fb874f285eb
Sthembiso <sthembisosotsha@email.com>
2025-06-15 09:34:51 +0200

    footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,14 @@
-
 import { Link } from "react-router-dom";
-import { Mail, Phone, MapPin, Facebook, Twitter, Linkedin, Github } from "lucide-react";
-import icon_1 from '/JIQ_white_logo_no_bg.png'
+import {
+  Mail,
+  Phone,
+  MapPin,
+  Facebook,
+  Twitter,
+  Linkedin,
+  Github,
+} from "lucide-react";
+import icon_1 from "/JIQ_white_logo_no_bg.png";
 
 const Footer = () => {
   return (
@@ -22,19 +29,29 @@ const Footer = () => {
               </Link>
             </div>
             <p className="text-white mb-4 max-w-md">
-              Empowering African businesses with simple, impactful technology solutions.
-              From web applications to AI automation, we make tech work for you.
+              Empowering African businesses with simple, impactful technology
+              solutions. From web applications to AI automation, we make tech
+              work for you.
             </p>
             <div className="flex space-x-4">
-              <a href="https:www.facebook.com/JuveniQ" className="text-white hover:text-primary transition-colors">
+              <a
+                href="https:www.facebook.com/JuveniQ"
+                className="text-white hover:text-primary transition-colors"
+              >
                 <Facebook size={20} />
               </a>
 
-              <a href="https://github.com/JuveniQ/" className="text-white hover:text-primary transition-colors">
+              <a
+                href="https://github.com/JuveniQ/"
+                className="text-white hover:text-primary transition-colors"
+              >
                 <Github size={20} />
               </a>
-              
-              <a href="https://www.linkedin.com/company/juveniq" className="text-white hover:text-primary transition-colors">
+
+              <a
+                href="https://www.linkedin.com/company/juveniq"
+                className="text-white hover:text-primary transition-colors"
+              >
                 <Linkedin size={20} />
               </a>
             </div>
@@ -45,22 +62,34 @@ const Footer = () => {
             <h3 className="text-lg font-semibold mb-4">Quick Links</h3>
             <ul className="space-y-2">
               <li>
-                <Link to="/about" className="text-grey hover:text-primary transition-colors">
+                <Link
+                  to="/about"
+                  className="text-grey hover:text-primary transition-colors"
+                >
                   About Us
                 </Link>
               </li>
               <li>
-                <Link to="/services" className="text-grey hover:text-primary transition-colors">
+                <Link
+                  to="/services"
+                  className="text-grey hover:text-primary transition-colors"
+                >
                   Services
                 </Link>
               </li>
               <li>
-                <Link to="/portfolio" className="text-grey hover:text-primary transition-colors">
+                <Link
+                  to="/portfolio"
+                  className="text-grey hover:text-primary transition-colors"
+                >
                   Portfolio
                 </Link>
               </li>
               <li>
-                <Link to="/contact" className="text-grey hover:text-primary transition-colors">
+                <Link
+                  to="/contact"
+                  className="text-grey hover:text-primary transition-colors"
+                >
                   Contact
                 </Link>
               </li>
@@ -74,23 +103,38 @@ const Footer = () => {
               <li className="flex items-start">
                 <Phone size={16} color="white" className="mr-2 text-primary mt-1" />
                 <span className="text-tertiary">
-                  <a href="tel:+27607431268" className="hover:text-primary transition-colors whitespace-nowrap">
-                  +27 60 743 1268
+                  <a
+                    href="tel:+27607431268"
+                    className="hover:text-primary transition-colors whitespace-nowrap"
+                  >
+                    +27 60 743 1268
                   </a>
                 </span>
               </li>
               <li className="flex items-start">
                 <Phone size={16} color="white" className="mr-2 text-primary mt-1" />
                 <span className="text-tertiary">
-                  <a href="tel:+27783322419" className="hover:text-primary transition-colors whitespace-nowrap">
-                  +27 78 332 2419
+                  <a
+                    href="tel:+27783322419"
+                    className="hover:text-primary transition-colors whitespace-nowrap"
+                  >
+                    +27 78 332 2419
                   </a>
                 </span>
               </li>
               <li className="flex items-start">
                 <MapPin size={16} color="white" className="mr-2 text-primary mt-1" />
+                <span className="text-tertiary">eMalahlani, South Africa</span>
+              </li>
+              <li className="flex items-start">
+                <Mail size={16} color="white" className="mr-2 text-primary mt-1" />
                 <span className="text-tertiary">
-                  eMalahlani, South Africa
+                  <a
+                    href="mailto:juveniq105@gmail.com"
+                    className="hover:text-primary transition-colors"
+                  >
+                    juveniq105@gmail.com
+                  </a>
                 </span>
               </li>
             </ul>
